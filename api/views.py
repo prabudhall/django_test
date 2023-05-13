bf1bf607a06206c1a8763430efcd2118d61a4ec8
Prabudh Kumar <prabudh2002@gmail.com>
2023-05-13 20:04:16 +0530

    Added success message on deleting the data and API contract

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import AllowAny
 from api.models import ItemsList
 from api.serializers import ItemSerializer
 from django.http import JsonResponse
+from django.urls.exceptions import NoReverseMatch
 # Create your views here.
 class ItemViewSet(viewsets.ModelViewSet):
 
@@ -31,7 +32,7 @@ class ItemViewSet(viewsets.ModelViewSet):
             elif amount is not None:
                 queryset = queryset.filter(amount=amount)
         else:
-            return JsonResponse({'error: cannot see in other method than GET'})
+            return JsonResponse({'error': 'cannot see in other method than GET'})
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -46,23 +47,34 @@ class ItemViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def update(self, request, *args, **kwargs):
-        if len(request.query_params) > 0 and request.method != 'GET':
-            raise ValidationError({'error': 'cannot see in other method than GET'})
+        try:
+            if len(request.query_params) > 0 and request.method != 'GET':
+                raise ValidationError({'error': 'cannot see in other method than GET'})
 
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data)
+        except ValidationError as e:
+            print('\n\n\n\n\n\n')
+            return Response({'error': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print('\n\n\n\n\n\n')
+            if 'APPEND_SLASH' in e:
+                return Response({'error': 'Please include a trailing slash in the URL.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'error': 'unexpected'}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         if len(request.query_params) > 0 and request.method != 'GET':
             raise ValidationError({'error': 'cannot see in other method than GET'})
-        
+
         instance = self.get_object()
         self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'delete': f'deleted {kwargs["pk"]} successfully'}, status=status.HTTP_204_NO_CONTENT)
 
 # in the new version it is required to write the queryset in the begin. if you need to do the changes then in the urls.py
 # where you are setting the url there you have to add the parameter basename='model_name'
